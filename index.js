cb875c13da4d086976f3f087097829f20fe91264
Rehan-ramzan-ali <ra6313002@gmail.com>
2025-07-28 21:21:09 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+// _______1.alert:__________//
+
+var names= " my name is Rehan"
+var greet= "Hello! "+ names
+alert (greet)
+// __________2.Variables for Numbers:_________//
+
+let ag="17"
+let gret="I am " + ag + " years old"
+alert(gret)
+
+// ________3.Variables for Strings:__________//
+
+var city="mey website"
+var grt=("Wellcom to "+ city)
+alert(grt)
+
+
+// ________4.Math Expressions:__________//
+
+var x="10"
+var y="5"
+
+console.log(x+y)
+console.log(x/y)
+console.log(x%y)
+// ________5.Prompt:________//
+alert("Tell me about you")
+
+let nam= prompt("what is your name","Name")
+alert("Hello! " + nam);
+
+let age= prompt("what is your age","Age")
+alert("Hello! " + "you are in good age");
+
+let nub= Number(prompt("Phone Number","92+0000000000"))
+alert("Thanks! for give me a number")
+// let nub2=Number(prompt("click","00"))
+// let result=(nub+nub2)
+// console.log(result)
+
+let color = prompt("what is your  favorite colour")
+alert("Wow! " + "I like also this color")
+
+//__________6.If Statement and Comparison Operators:_________ //
+
+let ummer=Number(prompt("Enter your age"))
+
+if (age<30){
+  console.log("You still young man")
+}
+else{
+  console.log("Your age is grerater than 30")}
